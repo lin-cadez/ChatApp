7d7f392eb41e9c42e82e3563c9988eb930c61245
jakecernet <jaka.cernetic@students.vegova.si>
2024-02-09 19:05:08 +0100

    neki

--- server/server.js
+++ server/server.js
@@ -5,15 +5,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api", (req, res) => {
-  res.json({
-    message: "Welcome to the API",
-    error: false
-  });
+	res.json({
+		message: "Welcome to the API",
+		error: false,
+	});
+});
 
+app.get("/api/jakec", (req, res) => {
+	res.json({
+		message: "Sup lil nigga",
+		error: false,
+	});
 });
 
 app.listen(5000, () => {
-  console.log("server started on port 5000");
+	console.log("server started on port 5000");
 });
 
 export default app;
