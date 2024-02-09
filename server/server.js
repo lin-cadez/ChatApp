bd160987acacb5d59ef7f266d0ac800cfa9a4815
Tilen <tilensimencthedev@gmail.com>
2024-02-09 19:03:24 +0100

    Trenutno Leto

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 import express from "express";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api", (req, res) => {
-  res.send("Hello World");
+  res.json({
+    message: "Welcome",
+    error: false
+  });
 
 });
+
+app.get("/api/leto", (req,res) => {
+  const year = new Date();
+  
+  res.json({
+    
+    
+    message:"leto: " + year.getFullYear(),
+    error: false
+  });
+});
+
 
 app.listen(5000, () => {
   console.log("server started on port 5000");
