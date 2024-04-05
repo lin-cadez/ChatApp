b4a1720e221d06c6a0f5185d3d973727dbdeb9fe
jakecernet <jaka.cernetic@students.vegova.si>
2024-04-05 19:15:13 +0200

    ui za message

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import "./home.css";
 
 function Home() {
 	return (
 		<div>
-			<h1>Home</h1>
-			<p>Home page body content</p>
+			<h1>ChatApp</h1>
+			<section className="chat-window">
+				<div className="messages">
+					<div className="other-message">
+						<div className="message">Hello!</div>
+					</div>
+					<div className="self-message">
+						<div className="message">Hi!</div>
+					</div>
+				</div>
+				<div className="input-field">
+					<input type="text" placeholder="Start typing..." />
+					<button>Send</button>
+				</div>
+			</section>
 		</div>
 	);
 }
